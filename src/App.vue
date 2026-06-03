ee52e7fe3312f164d7468bd5d658ac7e1d836770
josue <josuefig19@gmail.com>
2026-06-03 11:33:28 -0600

    feat: instalar Tailwind CSS y realizar configuración inicial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="container mx-auto mt-20">
+    <Header/>
+  </div>
 </template>
